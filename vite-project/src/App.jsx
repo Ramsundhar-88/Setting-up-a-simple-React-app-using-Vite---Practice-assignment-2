8ee33b52ada94b81051ae56d9cfa75cf6fcce9e3
Ramsundhar-88 <Ramsundhar.sades@gmail.com>
2025-02-19 10:29:03 +0530

    Details added

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px",background:"lightgray" }}>
+      <h1>Ramsundhar</h1>
+      <p>Contact: ramsundhar@sades@gmail.com</p>
+      <p>Phone: 6381476307</p>
+    </div>
+  );
+};
+
+export default Home;
